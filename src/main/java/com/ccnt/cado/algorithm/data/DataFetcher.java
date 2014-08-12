7dbf215a3acee43936f055b058e807e2e60f677d
zhoujinsong <463763777@qq.com>
2014-08-12 10:39:35 +0800

    test

--- src/main/java/com/ccnt/cado/algorithm/data/DataFetcher.java
+++ src/main/java/com/ccnt/cado/algorithm/data/DataFetcher.java
@@ -106,6 +106,7 @@ public class DataFetcher {
 			}
 			Unit staticUnit = new Unit(staticCpu, staticMemory, staticIO, -1);
 			vmCache.put((Integer)hostInfo.get("_id"), staticUnit);
-		}	
+		}
+		System.out.println("test");
 	}
 } 
