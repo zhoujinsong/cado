15d67b1712747f07e808be32ff0a0cba97178893
ssontheway <lisha.20082008@163.com>
2014-08-12 10:20:43 +0800

    data fetch test

--- src/main/java/com/ccnt/cado/algorithm/data/DataFetcher.java
+++ src/main/java/com/ccnt/cado/algorithm/data/DataFetcher.java
@@ -10,10 +10,50 @@ public class DataFetcher {
 	public List<VM> getVMsData(){
 		//≤‚ ‘ ˝æ›
 		List<VM> vms = new ArrayList<VM>();
+		VM vm1 = new VM(1,new Unit(8,512,10,10), new Unit(1.7,400,5,7));
+		VM vm2 = new VM(2,new Unit(4,1024,20,20), new Unit(3.5,800,5,7));
+		VM vm3 = new VM(3,new Unit(8,2048,40,40), new Unit(7.5,1900,5,7));
+
+		vms.add(vm1);
+		vms.add(vm2);
+		vms.add(vm3);
 		
 		return vms;
 	}
 	public List<Deploy> getDeployUnitsData(){
-		return null;
+		List<Deploy> deploys = new ArrayList<Deploy>();
+		//test migriate
+		//vm1 
+		/*Deploy dp1 = new Deploy(new Unit(0.5,200, 3, 3), 1, 1);
+		Deploy dp2 = new Deploy(new Unit(1,200, 2,4),1,2);
+		
+		//vm2
+		Deploy dp3 = new Deploy(new Unit(2,500, 3, 3), 2, 3);
+		Deploy dp4 = new Deploy(new Unit(1.5,300, 2,4),2,4);
+		
+		//vm3
+		Deploy dp5 = new Deploy(new Unit(4,1500, 3, 3), 3, 5);
+		Deploy dp6 = new Deploy(new Unit(3.5,400, 2,4),3,6);*/
+		
+		//test merge
+		Deploy dp1 = new Deploy(new Unit(0.5,200, 3, 3), 1, 1);
+		Deploy dp2 = new Deploy(new Unit(1,200, 2,4),1,2);
+		
+		//vm2
+		Deploy dp3 = new Deploy(new Unit(2,500, 3, 3), 2, 3);
+		Deploy dp4 = new Deploy(new Unit(1.5,300, 2,4),2,4);
+		
+		//vm3
+		Deploy dp5 = new Deploy(new Unit(0.5,200, 3, 3), 3, 5);
+		Deploy dp6 = new Deploy(new Unit(0.5,400, 2,4),3,6);
+		
+		deploys.add(dp1);
+		deploys.add(dp2);
+		deploys.add(dp3);
+		deploys.add(dp4);
+		deploys.add(dp5);
+		deploys.add(dp6);
+		
+		return deploys;
 	}
 } 
